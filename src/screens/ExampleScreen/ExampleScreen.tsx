b4065c897ebecfba362d83faee5f6885cd7fe964
dmtshirmanov <dmtshirmanov@MacBook-Pro-dmtshirmanov.local>
2023-05-07 23:40:17 +0400

    Add taliwindcss in react native

--- src/screens/ExampleScreen/ExampleScreen.tsx
+++ src/screens/ExampleScreen/ExampleScreen.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
-import {Text} from 'react-native';
+import {Text, View} from 'react-native';
 import * as UI from 'shared/ui';
 
 export const ExampleScreen = () => (
   <UI.Container>
-    <Text>I'm example secreen</Text>
+    <View className="bg-neutral-300 dark:bg-slate-900 flex-1 items-center justify-center">
+      <Text className="font-bold">I'm example secreen</Text>
+    </View>
   </UI.Container>
 );
--- src/shared/ui/Layouts/Container.tsx
+++ src/shared/ui/Layouts/Container.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {StyleSheet} from 'react-native';
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Container = ({children}: Props) => (
-  <SafeAreaView style={styles.container}>{children}</SafeAreaView>
+  <SafeAreaView className="flex-1">{children}</SafeAreaView>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
